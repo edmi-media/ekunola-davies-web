cbb9419c11829d3b8d3a4c5a623953fcbdea8063
Ekunola Davies <gtisocialmediateam@gmail.com>
2024-04-11 10:56:57 +0100

    chore: bilerplate done

--- src/components/shared/footer/footer.tsx
+++ src/components/shared/footer/footer.tsx
@@ -3,5 +3,5 @@ import React from "react"
 import styles from "./footer.module.scss"
 
 export const Footer = () => {
-	return <footer className={styles.Footer}>Footer</footer>
+	return <footer className={styles.Footer}></footer>
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,14 @@
 import Image from "next/image"
 
 import { Appbar, Footer, Seo } from "@/components/shared"
+import styles from "./style.module.scss"
 
 export default function Home() {
 	return (
 		<>
 			<Seo title="Home" />
 			<Appbar />
-			<main className="flex min-h-screen w-full flex-col items-center justify-center p-24">
-				<div className="relative aspect-[3.20/1] w-full max-w-[80%]">
-					<Image
-						src="/images/logo-dark.png"
-						alt="edmi Logo"
-						fill
-						sizes="(max-width) 100%"
-						className="h-full w-full"
-						priority
-					/>
-				</div>
-			</main>
+			<main className={styles.Main}></main>
 			<Footer />
 		</>
 	)
